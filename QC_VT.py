e80b7c75054f81a2f53cddfcfbba35baf15d8e81
Tommy Fauvell <tfauvell@users.noreply.github.com>
2019-03-05 16:43:53 -0500

    updated for Dev Summit 2019

--- QC_VT.py
+++ QC_VT.py
@@ -11,6 +11,7 @@
 #   Size converter: https://stackoverflow.com/a/14822210
 #
 # To do:
+#   Add file check for .pbf files
 #   Parameterize inputs
 #   Create polygon selection sql clause
 # ------------------------------------------------------------------------------
@@ -18,8 +19,21 @@ import os
 import operator
 import sys
 import math
-#vt = r'C:\Projects\_Esri Conferences\Dev Summit 2019\VT_QC\VTPK\QC\Update1\p12\tile'
-vt = r'C:\Projects\Census Basemap Review\VTPK\QC\Unpacked_Current\p12\tile'
+
+"""
+    QC_VT.py will analyse the contents of a vector tile package (VTPK) and 
+    generate a report based on the uncompressed file sizes of the .PBF files 
+    found in the cache structure. Make sure to extract the contents of your
+    VTPK with the 'extract package' geo-processing tool in ArcGIS Pro prior to
+    running this script to analyze .PBF payload sizes vs .bundle file sizes.
+
+    USAGE: The 'vt' parameter should be the absolute path to the 'tile' 
+    directory within the extracted VTPK structure. Adjust the max_size 
+    parameter as needed. 150KB is a conservative estimate based on average
+    512x512 raster tile payload sizes. 
+"""
+
+vt = r'c:\<path_to_your_extracted_vtpk>\p12\tile'
 max_size = 150  # KB
 
 statsD = {}
@@ -47,15 +61,22 @@ statsLookUp[9] = "[ larger than 1MB ]"
 
 
 def pretty_stats(x, y):
+    """
+        Convert file counts and total file counts into an ascii graph 
+        indicating % of total files found.
+    """
     x = float(x)
     if x == 0:
         m = 0
     else:
-        m = math.trunc(math.ceil((x/y)*10))
-    return (" [" + '#'*m + '.'*(10 - m) + "] ")
+        m = math.trunc(math.ceil(((x/y) * 100) / 5))
+    return (" [" + '#'*m + '.'*(20 - m) + "] ")
 
 
 def group_size(fSize):
+    """
+        Group file sizes into range categories.
+    """
     if fSize < 1024:
         return 1
     elif fSize < 16384:
@@ -77,6 +98,9 @@ def group_size(fSize):
 
 
 def convert_units(size_bytes):
+    """
+        Convert size in bytes to human readable file size notation.
+    """
     if size_bytes == 0:
         return "0B"
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
@@ -87,8 +111,9 @@ def convert_units(size_bytes):
 
 
 def main():
-    # generator - walk all file paths within vt
+    print "Analyzing tileset...\n"
 
+    # generator - walk all file paths within vt
     all_files = (os.path.join(basedir, filename)
                  for basedir, dirs, files in os.walk(vt) for filename in files)
 
@@ -96,13 +121,14 @@ def main():
     files_and_sizes = ((path, os.path.getsize(path)) for path in all_files)
     sorted_files_with_size = sorted(
         files_and_sizes, key=operator.itemgetter(1), reverse=True)
+    print "Collecting statistics..."
     min_PBF = min(sorted_files_with_size, key=operator.itemgetter(1))
     max_PBF = max(sorted_files_with_size, key=operator.itemgetter(1))
 
     fCount = 0
     flaggedPBF = list()
     totalSize = 0
-
+    
     for filePath in sorted_files_with_size:
         # flag large files and collect stats
         totalSize += filePath[1]
@@ -110,23 +136,25 @@ def main():
         if filePath[1]/1024 >= max_size:
             flaggedPBF.append((filePath[0], filePath[1]))
         fCount += 1
-
-    print "\n--------------------------------------"
-    print ("TILESET STATS: \n" + str(fCount) + " tiles, smallest tile: " + convert_units(min_PBF[1]) +
+    
+    print "\n--------------------------------------------------------"
+    print ("TILESET STATS: \n" + str('{0:,}'.format(fCount)) + " tiles, smallest tile: " + convert_units(min_PBF[1]) +
            ", largest tile: " + convert_units(max_PBF[1]) + ", total size: " + convert_units(totalSize))
-    print "[    size range   ] [  graph   ] # of files"
+    print "[    size range   ] [  % of total files  ] # of files"
 
     for key, val in statsD.items():
-        print (statsLookUp[key] + pretty_stats(val, fCount) + str(val))
+        print (statsLookUp[key] + pretty_stats(val, fCount) + str('{0:,}'.format(val)))
 
     if len(flaggedPBF) >= 1:
-        print "\nWARNING! Tileset contains tiles larger than %s KB" % (max_size)
+        print "\nWARNING! Tileset contains %s tiles larger than %s KB" % (
+            len(flaggedPBF), max_size)
         for key, val in flaggedPBF:
             # print f, s 'First: {0[0]}, Second: {0[1]}'.format(flag)
             print key + "  " + convert_units(val)
     else:
-        print "No tiles found larger than %s KB" % (max_size)
-    print "--------------------------------------\n"
+        print "\nNo tiles found larger than %s KB" % (max_size)
+    print "--------------------------------------------------------\n"
+
     # end main
 
 
